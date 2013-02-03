8f5f18d4e653a580d33241d801e672bc2d8c301b
Jeremy Wright <jeremy@codestrokes.com>
2013-02-02 19:09:38 -0700

    The OTP Tool is working, now need to build the encryptor

---
+++ Lab/Client2/OTP_Tool/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 3);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(278, 38);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonA, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button10, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button_calculate, 2, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonA
+            // 
+            this.buttonA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonA.Location = new System.Drawing.Point(97, 209);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(88, 49);
+            this.buttonA.TabIndex = 12;
+            this.buttonA.Text = "0";
+            this.buttonA.UseVisualStyleBackColor = true;
+            this.buttonA.Click += new System.EventHandler(this.buttonA_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(3, 101);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 48);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(97, 101);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 48);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(191, 101);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 48);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Location = new System.Drawing.Point(3, 155);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(88, 48);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Location = new System.Drawing.Point(97, 155);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 48);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Location = new System.Drawing.Point(191, 155);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 48);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 48);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(97, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 48);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(191, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 48);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button10
+            // 
+           
+            this.button10.Location = new System.Drawing.Point(3, 209);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(88, 47);
+            this.button10.TabIndex = 22;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Dock = System.Windows.Forms.DockStyle.Fill;
+            
+            this.button_calculate.Location = new System.Drawing.Point(191, 209);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(90, 49);
+            this.button_calculate.TabIndex = 11;
+            this.button_calculate.UseVisualStyleBackColor = true;
+            this.button_calculate.Click += new System.EventHandler(this.button_calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "CSE543 :: OTP";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_calculate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button10;
+
+    }
+}
+
---
+++ Lab/Client2/OTP_Tool/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            currentinput = new StringWithEvent();
+            currentinput.PropertyChanged += currentinput_PropertyChanged;
+        }
+
+        void currentinput_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Hash")
+            {
+                var tempFont = textBox1.Font;
+                textBox1.Font = new Font(tempFont, FontStyle.Italic);
+                textBox1.Text = currentinput.Hash;
+            }
+            else if (e.PropertyName == "MyString" || currentinput.MyString != null)
+            {
+                var tempFont = textBox1.Font;
+                textBox1.Font = new Font(tempFont, FontStyle.Regular);
+                textBox1.Text = currentinput.MyString;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "9";
+        }
+
+        private void buttonA_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString += "0";
+        }
+
+        private void button_calculate_Click(object sender, EventArgs e)
+        {
+            if (currentinput.MyString == null)
+                return;
+                        
+            HMACSHA1 hmac_sha1 = new HMACSHA1(Encoding.ASCII.GetBytes("VjXb38Zn7H6wnvBlczkY"));
+            var salt = SHA1.Create().ComputeHash(Encoding.ASCII.GetBytes(currentinput.MyString));
+            hmac_sha1.Initialize();
+
+            var hashBytes = hmac_sha1.ComputeHash(salt);
+            
+            // Use a bitwise operation to get a representative binary code from the hash
+            // Refer section 5.4 at http://tools.ietf.org/html/rfc4226#page-7            
+            int offset = hashBytes[19] & 0xf;
+            int binaryCode = (hashBytes[offset] & 0x7f) << 24
+                | (hashBytes[offset + 1] & 0xff) << 16
+                | (hashBytes[offset + 2] & 0xff) << 8
+                | (hashBytes[offset + 3] & 0xff);
+            
+            int otp = binaryCode % (int)Math.Pow(10, 8); // where 8 is the password length
+
+            currentinput.Hash = otp.ToString().PadLeft(8, '0');
+        }
+
+        private StringWithEvent currentinput;
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            currentinput.MyString = null;   
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Hello.");
+        }
+
+       
+    }
+
+    public class StringWithEvent : INotifyPropertyChanged
+    {
+        private string myString;
+
+        public string MyString
+        {
+            get
+            { return myString; }
+            set
+            {
+                myString = value;
+                OnPropertyChanged("MyString");
+            }
+        }
+
+        public string Hash {
+            get
+            {
+                return hash;
+            }
+            set
+            {
+                hash = value;
+                myString = null; //Clear the private string.
+                OnPropertyChanged("Hash");
+            }
+        }
+        private string hash;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            { handler(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
